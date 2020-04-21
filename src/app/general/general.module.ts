2b17c40b7cb3b354bae4aeecc66465c138ba26ac
NathalieSong <yuejuansong@outlook.com>
2020-04-21 18:19:09 +0800

    Init modules

---
+++ src/app/general/general.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin/signin.component';
+import { SignoutComponent } from './signout/signout.component';
+
+
+
+@NgModule({
+  declarations: [SigninComponent, SignoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GeneralModule { }
